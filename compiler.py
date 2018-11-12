5ee57e90939a2bed254c5def2c2770f58b611b1b
K.D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2018-11-12 21:52:30 +0530

    Programmed a compiler

---
+++ compiler.py
@@ -0,0 +1,76 @@
+# All available commands
+ADD = "ADD"
+NEG = "NEG"
+MOVI = "MOVI"
+JZR = "JZR"
+SUB = "SUB"
+CLR = "CLR"
+MOV = "MOV"
+NOP = "NOP"
+
+# Opcodes for commands
+opcodes = {
+    ADD: "000",
+    NEG: "001",
+    MOVI: "010",
+    JZR: "011",
+    SUB: "100",
+    CLR: "101",
+    MOV: "110",
+    NOP: "111"
+}
+
+# Define inputs here
+inputs = [
+    "NEG R",
+    "ADD R4 R5",
+    "MOVI R3"
+    ]
+
+
+for i in xrange(len(inputs)):
+    raw_data = inputs[i].upper().split()
+    try:
+        # Extract opcode
+        encoded_opcode = raw_data[0]
+        # Take binary opcode
+        opcode = opcodes[encoded_opcode]
+        
+        # Selectively put opcodes
+        if encoded_opcode in (NOP, ):
+            # No operands
+            operands = ["-", "-", "-"]
+        if encoded_opcode in (CLR, NEG):
+            # Only Ra operands
+            operands = [raw_data[1], "-", "-"]
+        if encoded_opcode in (ADD, SUB, MOV):
+            # Only Ra and Rb operands
+            operands = [raw_data[1], raw_data[2], "-"]
+        if encoded_opcode in (MOVI, JZR):
+            # Only Ra and d operands
+            operands = [raw_data[1], "-", raw_data[2]]
+        
+        # Extract operands
+        encoded_ra = operands[0]
+        encoded_rb = operands[1]
+        encoded_literal = operands[2]
+        
+        # Convert them to binary
+        ra = "000" if encoded_ra == "-" else \
+                        bin(int(encoded_ra[1]))[2:].zfill(3)
+        
+        rb = "000" if encoded_rb == "-" else \
+                        bin(int(encoded_rb[1]))[2:].zfill(3)
+        
+        literal = "0000" if encoded_literal == "-" else \
+                        bin(int(encoded_literal))[2:].zfill(4)
+        
+        # Output
+        print opcode + ra + rb + literal
+    except:
+        # Syntax error catcher
+        print "Syntax Error in line {}\nLine:\t{}\n".format(i, inputs[i]) 
+        break
+
+
+
