4177876fddcc664d8c20131d74197ac586f3aff8
K.D. Sunera Avinash Chandrasiri <34407567+kdsuneraavinash@users.noreply.github.com>
2018-11-13 08:53:37 +0530

    Update Instruction_Decoder.vhd
    Can simulate in https://www.edaplayground.com

--- Nano Processor/Nano Processor.srcs/sources_1/new/Instruction_Decoder.vhd
+++ Nano Processor/Nano Processor.srcs/sources_1/new/Instruction_Decoder.vhd
@@ -22,15 +22,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Instruction_Decoder is
     Port ( Inst : in STD_LOGIC_VECTOR (16 downto 0);
            CLK : in STD_LOGIC;
@@ -44,12 +35,7 @@ entity Instruction_Decoder is
            JMP : out STD_LOGIC);
 end Instruction_Decoder;
 
-architecture Behavioral of Instruction_Decoder is
-    signal Opcode: std_logic_vector(2 downto 0);
-    signal Ra: std_logic_vector(2 downto 0);
-    signal Rb: std_logic_vector(2 downto 0);
-    signal d: std_logic_vector(7 downto 0);
-    
+architecture Behavioral of Instruction_Decoder is    
     constant OP_ADD: std_logic_vector(2 downto 0):= "000";
     constant OP_NEG: std_logic_vector(2 downto 0):= "001";
     constant OP_MOVI: std_logic_vector(2 downto 0):= "010";
@@ -59,21 +45,39 @@ architecture Behavioral of Instruction_Decoder is
     constant OP_MOV: std_logic_vector(2 downto 0):= "110";
     constant OP_NOP: std_logic_vector(2 downto 0):= "111";
 begin
-    
-    Opcode <= Inst(16 downto 14);
-    Ra <= Inst(13 downto 11);
-    Ra <= Inst(10 downto 8);
-    d <= Inst(7 downto 0);
-    
-   
-                    Mux_A <= Ra;
-                Mux_B <= Rb;
-                SUB <= '0';
-                
-                -- unimportant
-                LSB <= "00000000";
-                Reg_En <= "000";
-                LD <= '1';
-                JMP <= '0';
 
-end Behavioral;
+	process (CLK)
+    	-- variable l : line;
+  	begin
+    if rising_edge(CLK) then
+    
+      case Inst(16 downto 14) is
+        when OP_ADD =>
+          -- write (l, String'("OP_ADD"));
+          -- writeline (output, l);
+          Mux_A <= Inst(13 downto 11);
+          Mux_B <= Inst(10 downto 8);
+          
+        when OP_NEG =>
+          -- write (l, String'("OP_NEG"));
+          -- writeline (output, l);
+          Mux_A <= Inst(13 downto 11);
+          Mux_B <= Inst(10 downto 8);
+          
+        when OP_MOVI => 
+          -- write (l, String'("OP_MOVI"));
+          -- writeline (output, l);
+          Mux_A <= Inst(13 downto 11);
+          Mux_B <= Inst(10 downto 8);
+          
+        when others =>
+          -- write (l, String'("other"));
+          -- writeline (output, l);
+          Mux_A <= Inst(13 downto 11);
+          Mux_B <= Inst(10 downto 8);
+          
+      end case;
+    end if;
+  end process;
+
+end Behavioral; 
